5cf7d149b82a82c86910ff4ae226c074e2502295
Krzysztof Franek <k.franek@tme.pl>
2022-06-04 14:01:10 +0200

    Step 1.8

--- src/RepoMenu.js
+++ src/RepoMenu.js
@@ -1,23 +1,19 @@
 import React, {useEffect} from "react";
-import useIterator from "./useIterator";
-export default function RepoMenu({
-                                     repositories,
-                                     onSelect = f => f
-                                 }) {
+import RepositoryReadme from "./RepositoryReadme";
+import { useIterator } from "./useIterator";
+
+export default function RepoMenu({ repositories, login }) {
     const [{ name }, previous, next] = useIterator(
         repositories
     );
-
-    useEffect(() => {
-        if (!name) return;
-        onSelect(name);
-    }, [name]);
-
     return (
-        <div style={{ display: "flex" }}>
-            <button onClick={previous}>&lt;</button>
-            <p>{name}</p>
-            <button onClick={next}>&gt;</button>
-        </div>
+        <>
+            <div style={{ display: "flex" }}>
+                <button onClick={previous}>&lt;</button>
+                <p>{name}</p>
+                <button onClick={next}>&gt;</button>
+            </div>
+            <RepositoryReadme login={login} repo={name} />
+        </>
     );
 }
