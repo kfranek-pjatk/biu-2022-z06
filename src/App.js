5ebd24f1c33305867245eea53a0c88da1e326e52
Krzysztof Franek <k.franek@tme.pl>
2022-06-04 10:48:26 +0200

    Step 2.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, {useState, useEffect} from "react";
-
-function GitHubUser({login}) {
-    const [data, setData] = useState();
-
-    useEffect(() => {
-        if (!login) return;
-        fetch(`https://api.github.com/users/${login}`)
-            .then(response => response.json())
-            .then(setData)
-            .catch(console.error);
-    }, [login]);
-
-    if (data)
-        return <pre>{JSON.stringify(data, null, 2)}</pre>;
-    return null;
-}
+import React from "react";
+const
+    tahoe_peaks = [
+        { name: "Freel Peak", elevation: 10891 },
+        { name: "Monument Peak", elevation: 10067 },
+        { name: "Pyramid Peak", elevation: 9983 },
+        { name: "Mt. Tallac", elevation: 9735 }
+    ];
 export default function App() {
-    return <GitHubUser login="mmiotk"/>;
+    return (
+        <ul>
+            {tahoe_peaks.map((peak, i) => (
+                <li key={i}>
+                    {peak.name} - {peak.elevation.toLocaleString()}ft
+                </li>
+            ))}
+        </ul>
+    );
 }
