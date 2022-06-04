64c450ae33139952fb7ed4cef2a5d8b8c3203410
Krzysztof Franek <k.franek@tme.pl>
2022-06-04 10:50:28 +0200

    Step 2.2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-const
-    tahoe_peaks = [
-        { name: "Freel Peak", elevation: 10891 },
-        { name: "Monument Peak", elevation: 10067 },
-        { name: "Pyramid Peak", elevation: 9983 },
-        { name: "Mt. Tallac", elevation: 9735 }
-    ];
+
+function List({ data = [], renderEmpty }) {
+    if (!data.length) return renderEmpty;
+    return <p>{data.length} items</p>;
+}
+
 export default function App() {
-    return (
-        <ul>
-            {tahoe_peaks.map((peak, i) => (
-                <li key={i}>
-                    {peak.name} - {peak.elevation.toLocaleString()}ft
-                </li>
-            ))}
-        </ul>
-    );
+    return <List renderEmpty={<p>This list is empty</p>} />;
 }
