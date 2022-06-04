86326e8c099331b0899bfe299556cf8023698ad2
Krzysztof Franek <k.franek@tme.pl>
2022-06-04 12:43:15 +0200

    Step 1.4

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-// App.js
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import {useFetch} from "./useFetch";
 
 function GitHubUser({ login }) {
-    const [data, setData] = useState();
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        if (!login) return;
-        setLoading(true);
-        fetch(`https://api.github.com/users/${login}`)
-            .then(data => data.json())
-            .then(setData)
-            .then(() => setLoading(false))
-            .catch(setError);
-    }, [login]);
-
+    const { loading, data, error } = useFetch(
+        `https://api.github.com/users/${login}`
+    );
     if (loading) return <h1>loading...</h1>;
     if (error)
         return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    if (!data) return null;
 
     return (
         <div className="githubUser">
             <img
                 src={data.avatar_url}
                 alt={data.login}
-                style={{width: 200}}
+                style={{ width: 200 }}
             />
             <div>
                 <h1>{data.login}</h1>
@@ -38,5 +26,10 @@ function GitHubUser({ login }) {
 }
 
 export default function App() {
-    return <GitHubUser login="mmiotk"/>;
+    const [login,] = useState("mmiotk");
+    return (
+        <>
+            <GitHubUser login={login} />
+        </>
+    );
 }
